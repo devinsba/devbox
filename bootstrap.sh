6501f34b4c8f5331ad6d247cbf15f32775a95489
Brian Devins-Suresh <badevins@gmail.com>
2022-09-15 09:53:03 -0400

    Change homebrew command

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,7 +41,7 @@ macos() {
   fi
 
   if ! brew commands > /dev/null 2>&1; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
   brew install git ansible lastpass-cli
